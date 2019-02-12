9187ac8497a880b645eef9c21e33587002d26d01
Alexandr Zinin <znalexandr@yandex.ru>
2019-02-12 16:33:08 +0300

    clean

--- src/pages/Todoist.js
+++ src/pages/Todoist.js
@@ -26,32 +26,15 @@ export default class App extends React.Component {
                     <div className="row no-gutters mb-3">
                         <div className="col-lg-6 pr-lg-2 mb-lg-0 mb-3">
                             <SearchPanel />
-                            {/* <SearchPanel 
-                                handleSearch={(name) => this.handleSearch(name)}
-                                search={this.state.search}
-                            /> */}
                         </div>
                         <div className="col-lg-6 text-center">
                             <AppFilter />
-                            {/* <AppFilter  filterTask={this.state.filterTask}
-                                        filterBtnChange={(action) => this.filterBtnChange(action)} /> */}
                         </div>
                     </div>
                     <div className="mb-3">
-                            <TodoList />
-                            {/* <TodoList tasks={tasks}
-                                        changeDoneHandler={ (id) => this.changeDoneHandler(id)}
-                                        changeImportantHandler={ (id) => this.changeImportantHandler(id)}
-                                        deleteItemHandler={ (id) => this.deleteItemHandler(id)}
-                            /> */}
-                        
+                        <TodoList />
                     </div>
                     <AddItem />
-                    {/* <AddItem 
-                        input={this.state.createItemTask}
-                        addInputChange={(name) => this.addInputChange(name)}
-                        addItemTask={this.addItemTask}
-                    /> */}
                 </Provider>     
             </React.Fragment>)
     }
